72f9c5af0261920fc3251c6f97247316756ba09c
danoseun <ayodejisomefun@yahoo.com>
2019-08-15 14:07:48 +0100

    Fix houndCI errors

--- src/tests/index.js
+++ src/tests/index.js
@@ -6,6 +6,6 @@ const testFunction = require('../utils');
 
 describe('Test function', function() {
     it('Test function should return welcome to Barefoot Nomad', function(){
-        assert.equal(testFunction(), 'Welcome to Barefoot Nomad!')
+        assert.equal(testFunction(), 'Welcome to Barefoot Nomad!');
     });
 });
