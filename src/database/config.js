e07020ebeb85399bddae8add954b65584064b2b0
bidemi oritunmise <bidoritunmise@gmail.com>
2019-08-21 06:41:09 -0700

    chore(sequelize):edit require to import for config
    [Finishes #167730617]

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import dotenv from 'dotenv'; 
+dotenv.config();
 
 const { DB_USERNAME, DATABASE, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } = process.env;
 module.exports = {
