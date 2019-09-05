6a09679e71e81378dcde26172fa84878f3f039c7
t4christ <bakaretemitayo7@gmail.com>
2019-09-05 10:27:37 +0100

    modify mail service

--- src/modules/email.js
+++ src/modules/email.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service:'sendgrid',
-  host:'smtp.sendgrid.net',
+  service:'gmail',
   auth: {
     user: process.env.EMAIL_ADD,
     pass: process.env.EMAIL_PASS
