63b418405cf4e4ffd9ca56a8ff9cec154a656427
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-09-13 23:39:18 +0100

    feature(notify-new-request): new travel requests
    - The push notification
    - The email notification
    - Create a notifications table
    - Install dependencies socket.io and auto-bind
    - Prepare the server
    - Set-up the project
    - Display New travel Requests
    - Add Notifications
    - Send Push and Email Notifications
    [Finishes: #167730641]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import auth from './authRoutes';
 import travel from './travelRoute';
 import comment from './commentRoutes';
+import notify from './notifyRoutes';
 
 
 export default (prefix, app) => {
-	app.use(prefix, auth);
-	app.use(prefix, travel);
-	app.use(prefix, comment);
+    app.use(prefix, auth);
+    app.use(prefix, travel);
+    app.use(prefix, comment);
+    app.use(prefix, notify);
 };
