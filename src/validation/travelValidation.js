b61c99ec19f89f34974d8305d7974f51da9580f8
bidemi oritunmise <34286667+coolbop1@users.noreply.github.com>
2019-09-16 10:29:23 +0100

    Update travelValidation.js

--- src/validation/travelValidation.js
+++ src/validation/travelValidation.js
@@ -89,8 +89,7 @@ export const validateResult = (req, res, next) => {
 };
 export const destinationCounts = (req, res, next) => {
   const { destination } = req.body;
-  let cityCount = destination.split("NEXT").length;
+  const cityCount = destination.split("NEXT").length;
   req.destinationCount = cityCount > 1 ? 1 : 0;
-  console.log(cityCount);
   return next();
 };
