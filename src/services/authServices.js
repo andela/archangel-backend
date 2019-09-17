1136830155c98f7fe695b3610d412ab9189b9492
Marshalheri <chizyberto@gmail.com>
2019-09-17 05:19:52 -0700

    ft(edited-request-notify): implement feature for notification on edited request - fixes hound violations and update the env.sample file [Fixes #167730653]

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -82,8 +82,8 @@ export const getUserProfileService = async (id) => {
 };
 
 // Get the user together with the department data of that user..
-export const findUserAndDepartment = async (id) => {
-  return await users.findOne({
+export const findUserAndDepartment = (id) => {
+  return users.findOne({
     where: { id },
     include: [departments]
   });
